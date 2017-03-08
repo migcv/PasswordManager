9c67ef5b9dc5b14dd0e2674e3dab3eeab9e7aef1
dharuq <dharuq@gmail.com>
2017-03-08 15:01:49 +0000

    create key pair and the certificate

--- pm-client/src/main/java/pm/Library.java
+++ pm-client/src/main/java/pm/Library.java
@@ -1,17 +1,35 @@
 package pm;
-import java.security.KeyStore;
+
+import java.security.*;
+import java.security.cert.X509Certificate;
 
 public class Library {
 
-	public void init(KeyStore ks){}
-	
-	public void register_user(){}
-	
-	public void save_password(byte[] domain, byte[] username, byte[] password){}
-	
-	public byte[] retrieve_password(byte[] domain, byte[] username){
+	public void init(char[] password, String alias, KeyStore... ks) {
+		CreateKey ck = new CreateKey();
+		if (ks.length == 0) {
+			try {
+				ck.generateKeyPair(password, alias);
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+		ck.verifyKeyStore();
+
+	}
+
+	public void register_user() {
+	}
+
+	public void save_password(byte[] domain, byte[] username, byte[] password) {
+	}
+
+	public byte[] retrieve_password(byte[] domain, byte[] username) {
 		return null;
 	}
-	
-	public void close(){}
+
+	public void close() {
+	}
 }
