f23f76e70a7c30cdaaf0bbd0773a48d19a85fac4
dharuq <dharuq@gmail.com>
2017-03-12 11:30:55 +0000

    Library almost done

--- pm-client/src/main/java/pm/Library.java
+++ pm-client/src/main/java/pm/Library.java
@@ -1,13 +1,37 @@
 package pm;
 
 import java.rmi.Naming;
+import java.rmi.RemoteException;
 import java.security.*;
 import java.security.cert.X509Certificate;
 
 public class Library {
+	
+	private ServerService server = null;
+	private KeyManagement ck = new KeyManagement();
+	
+	private void connectToServer(){
+		if (System.getSecurityManager() == null) {
+    		System.setSecurityManager(new SecurityManager());
+    	}
+    	else System.out.println("Já tem um cenas");
+    	
+    	try {
+    		
+    		server = (ServerService) Naming.lookup("//localhost:10000/ServerService");
+    		System.out.println("Encontrou o servidor");
+    		
+    	} catch (Exception e) {
+    		System.out.println("Houve problemas: " + e.getMessage());
+    	}
+    	
+	}
+	
 
 	public void init(char[] password, String alias, KeyStore... ks) {
-		KeyManagement ck = new KeyManagement();
+		
+		connectToServer();
+		
 		if (ks.length == 0) {
 			try {
 				ck.generateKeyPair(password, alias);
@@ -23,22 +47,42 @@ public class Library {
 		}
 	}
 
-	public void register_user(ServerService server) {
-		KeyManagement ck = new KeyManagement();
-		PublicKey pk = ck.getPk();
+	public void register_user() {
 		
-		//Nao entendo o que é que eles querem aqui
+		try {
+			server.register(ck.getPk());
+		} catch (RemoteException e) {
+			e.printStackTrace();
+		}
+		
 	}
 
 	public void save_password(byte[] domain, byte[] username, byte[] password) {
-		// Aqui tem que se fazer a cena da hash e enviar a password encriptada
+		
+		try {
+			server.put(ck.getPk(), domain, username, password);
+		} catch (RemoteException e) {
+			e.printStackTrace();
+		}
 	}
 
 	public byte[] retrieve_password(byte[] domain, byte[] username) {
-		return null;
+		byte[] password = null;
+		try {
+			 password = server.get(ck.getPk(), domain, username);
+		} catch (RemoteException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return password;
 	}
 
 	public void close() {
+		//It's better to leave it 
+		//the connection between RMI client and server is implicit. 
+		//The connection closes after a short idle period of time automatically.
+		//RMI's TCP connections are managed invisibly under the hood.
+		//Just let the stub be garbage-collected.
 	}
 	
 	
